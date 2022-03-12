7e335ba6982b86cf9f525ddc8a584866cea432e7
sitonumbis <sito8943@gmail.com>
2022-03-12 12:19:19 -0800

    title to logo

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // components
-import MemeShare, { Container, AppleDots } from "./MemeShare";
+import MemeShare, { Container, AppleDots, Title } from "./MemeShare";
 
 // icons
 import { ShareIcon } from "./components/Icons/icons";
@@ -14,6 +14,9 @@ const Template = () => {
     >
       <Container>
         <AppleDots />
+        <Title style={{ margin: 10 }} variant="h4">
+          Code Meme to Share
+        </Title>
         <Container className="flex justify-content-center align-items-center">
           <ShareIcon color="aliceblue" size="xx-large" />
         </Container>
