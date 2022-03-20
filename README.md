636c49519003922a7c9ca2f1b7e511be2c8e0ce5
SitoNumbis <sito8943@gmail.com>
2022-03-19 23:53:55 -0400

    added FileButton

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-npm# CodeMeme2Share@1.9.16
+npm# CodeMeme2Share@1.10.0
 
 Do you want to make your cool code shorts public?
 Just use:
---
+++ src/components/Button/FileButton/FileButton.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+// emotion
+import { css } from "@emotion/css";
+
+// styles
+import "./style.scss";
+
+const FileButton = (props) => {
+  const {
+    children,
+    id,
+    className,
+    name,
+    style,
+    type,
+    options,
+    ignoreDefault,
+    action,
+    mouseOver,
+    mouseLeave,
+    accept,
+  } = props;
+
+  const emotionCss = css(options);
+
+  return (
+    <label
+      onMouseEnter={mouseOver}
+      onMouseLeave={mouseLeave}
+      type={type}
+      id={id}
+      className={`${className} ${!ignoreDefault ? emotionCss : ""}`}
+      name={name}
+      style={style}
+    >
+      <input
+        type="file"
+        id={`file${id}`}
+        name={`file${name}`}
+        accept={accept}
+        onChange={action}
+      />
+      {children}
+    </label>
+  );
+};
+
+FileButton.defaultProps = {
+  className: "",
+  id: "",
+  name: "",
+  style: {},
+  options: {
+    fontSize: "0.875rem",
+    cursor: "pointer",
+    backgroundColor: "#2223335e",
+    border: "1px solid #22233366",
+    backdropFilter: "blur(4px)",
+    padding: "10px",
+    borderRadius: "0.5rem",
+    color: "aliceblue",
+    "&:hover": {
+      backgroundColor: "#6e3b70",
+      border: "1px solid #6e3b70",
+    },
+  },
+  type: "FileButton",
+  ignoreDefault: false,
+  action: null,
+  mouseOver: null,
+  mouseLeave: null,
+  accept: ".txt",
+};
+
+FileButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  style: PropTypes.objectOf(PropTypes.any),
+  className: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  options: PropTypes.objectOf(PropTypes.any),
+  ignoreDefault: PropTypes.bool,
+  action: PropTypes.func,
+  mouseOver: PropTypes.func,
+  mouseLeave: PropTypes.func,
+  accept: PropTypes.string,
+};
+
+export default FileButton;
