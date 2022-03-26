c064264504d57b226660cda99f1506a46b64bf6c
Sito Nimbus <40501794+SitoNumbis@users.noreply.github.com>
2022-03-26 18:41:14 -0400

    template

--- src/Template.jsx
+++ src/Template.jsx
@@ -4,15 +4,11 @@ import React from "react";
 import MemeShare, {
   Container,
   AppleDots,
-  Title,
-  FileButton,
   Notification,
   LinkButton,
 } from "./App";
 
 // images
-import react from "./assets/images/logo512.png";
-import Rotate from "./components/Container/Rotate/Rotate";
 import Loading from "./components/Loading/Loading";
 import Spinner from "./components/Loading/Spinner/Spinner";
 
